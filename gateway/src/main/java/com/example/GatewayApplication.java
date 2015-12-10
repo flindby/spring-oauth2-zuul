1280431780c7c37b541d51736f04ef749a032144
Filip Lindby <filip.lindby@gmail.com>
2015-12-10 14:20:01 +0100

    Moved OAuth2 SSO out to API Gateway (Zuul) instead of ui1 application

--- gateway/src/main/java/com/example/GatewayApplication.java
+++ gateway/src/main/java/com/example/GatewayApplication.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -12,31 +13,37 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateCustomizer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.security.web.session.SessionManagementFilter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.WebUtils;
 
 /**
- * 
+ * API Gateway 
  * @author Filip Lindby
  *
  */
 @SpringBootApplication
 @EnableZuulProxy
+@EnableOAuth2Sso
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-      
+    
     @Configuration
 	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -45,11 +52,16 @@ public class GatewayApplication {
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			http
+        	.authorizeRequests()
+        	//Allow access to all static resources without authentication
+        	.antMatchers("/","/**/*.html").permitAll()
+        	.anyRequest().authenticated()
+        	.and()
 				.csrf().csrfTokenRepository(csrfTokenRepository())
 			.and()
 				.addFilterAfter(csrfHeaderFilter(), SessionManagementFilter.class);
 			
-			http.httpBasic().disable();
+			//http.httpBasic().disable();
 		}
 
 		private Filter csrfHeaderFilter() {
@@ -81,3 +93,16 @@ public class GatewayApplication {
 		}
 	}
 }
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class WorkaroundRestTemplateCustomizer implements UserInfoRestTemplateCustomizer {
+
+	@Override
+	public void customize(OAuth2RestTemplate template) {
+		template.setInterceptors(new ArrayList<>(template.getInterceptors()));
+	}
+
+}
+
+
--- ui1/src/main/java/com/example/Ui1Application.java
+++ ui1/src/main/java/com/example/Ui1Application.java
@@ -1,18 +1,7 @@
 package com.example;
 
-import java.util.ArrayList;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateCustomizer;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 
@@ -20,44 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @SpringBootApplication
-@RestController
-@EnableOAuth2Sso
-public class Ui1Application extends WebSecurityConfigurerAdapter {
+public class Ui1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ui1Application.class, args);
 	}
-
-	// Should be accessed from :9999
-//		@RequestMapping("/user")
-//		@ResponseBody
-//		public Principal user(Principal user) {
-//			return user;
-//		}
-	
-	@Override
-	public void configure(HttpSecurity http) throws Exception {
-		
-		http
-			.logout()
-		.and()
-			.antMatcher("/**").authorizeRequests()
-			.antMatchers("/index.html", "/home.html", "/", "/login").permitAll()
-			.anyRequest().authenticated()
-	
-		// We use OAuth2 SSO, so we don't need basic auth
-		.and().httpBasic().disable();
-	}
-}
-
-//Remove this when upgrading to Spring Boot 1.3.1 (https://github.com/spring-projects/spring-boot/issues/4553)
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
-class WorkaroundRestTemplateCustomizer implements UserInfoRestTemplateCustomizer {
-
-	@Override
-	public void customize(OAuth2RestTemplate template) {
-		template.setInterceptors(new ArrayList<>(template.getInterceptors()));
-	}
-
 }
